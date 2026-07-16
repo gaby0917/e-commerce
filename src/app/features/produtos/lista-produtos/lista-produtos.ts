4354d889fdb1df602798dba35cdf609e62d1443e
Gabrielly Amaral <gabyamaral2805@gmail.com>
2026-07-16 17:17:37 -0300

    refactor: Ajustar sintaxe do set  para atualizar lista de produtos

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,9 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component} from '@angular/core';
+import { signal } from '@angular/core';
 import { Produto } from '../produto/produto';
+import { computed } from '@angular/core';
+import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
 
 @Component({
   selector: 'app-lista-produtos',
-  imports: [Produto],
+  imports: [Produto, PrecoFormatadoPipe],
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
@@ -24,4 +27,14 @@ export class ListaProdutos {
       {nome:'Playstation 5 Pro', preco:10000}
     ]);
   }
+  totalProdutos = computed(() => this.produtos().length);
+
+  valorTotal = computed(() => { return this.produtos().reduce
+    ((total, item) => total + item.preco, 0)});
+
+    substituirProdutos (){
+      this.produtos.set([
+        {nome: 'Arroz Fazenda', preco: 400},
+      ]);
+    }
 }
