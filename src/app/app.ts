9e723727fd9b30562a4a942c4e758c87d7d7b281
Gabrielly Amaral <gabyamaral2805@gmail.com>
2026-07-21 17:12:30 -0300

    refactor: Adicionado novo nome a Loja Virtual

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router'; //Remove inportação do RouterOutlet, pois no momento não estou usando no momento
 import { Produto } from './features/produtos/produto/produto'; 
-import {ListaProdutos} from './features/produtos/lista-produtos/lista-produtos';
+//import {ListaProdutos} from './features/produtos/lista-produtos/lista-produtos';
 
 @Component({
   selector: 'app-root',
@@ -11,5 +11,5 @@ import {ListaProdutos} from './features/produtos/lista-produtos/lista-produtos';
 })
 export class App {
   protected readonly title = signal('e-commerce');
-  nomeLoja = 'Pewiccon';
+  nomeLoja = 'Pewiccon'; //nome do e-commerce
 }
