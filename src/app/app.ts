14daea49e04ab40a866eab2d62dc939e1e5de762
Gabrielly Amaral <gabyamaral2805@gmail.com>
2026-07-21 14:23:41 -0300

    feat: Adiciona nome da loja no componente principal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,15 @@
 import { Component, signal } from '@angular/core';
-//!import { RouterOutlet } from '@angular/router'; //Remove inportação do RouterOutlet, pois no momento não estou usando no momento
+import { RouterOutlet, RouterLink } from '@angular/router'; //Remove inportação do RouterOutlet, pois no momento não estou usando no momento
 import { Produto } from './features/produtos/produto/produto'; 
 import {ListaProdutos} from './features/produtos/lista-produtos/lista-produtos';
 
 @Component({
   selector: 'app-root',
-  imports: [ListaProdutos],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
+  nomeLoja = 'Liwi';
 }
