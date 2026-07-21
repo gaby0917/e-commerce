a568bc50c7135388cbabd6543b0d2a6fcbc63f21
Gabrielly Amaral <gabyamaral2805@gmail.com>
2026-07-21 14:17:23 -0300

    Chore: Criar estrutura inicial da pasta carrinho, dentro do features

---
+++ src/app/features/carrinho/carrinho/carrinho.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrinho',
+  imports: [],
+  templateUrl: './carrinho.html',
+  styleUrl: './carrinho.css',
+})
+export class Carrinho {
+  //lógica do carrinho vira em aula futuras
+  //por ora, apenas exibe a página de carrinho
+}
